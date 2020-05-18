01f92480f20fbc4dd27913c5dd12262ede7e0ced
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-18 14:32:19 -0700

    set up state in the dashboard route

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import config from '../../config'
+import TokenService from '../../services/token-service'
 
 class DashboardRoute extends Component {
+
+  state = {
+    words: []
+  }
+
+  componentDidMount() {
+    fetch(`${config.API_ENDPOINT}/language`, {
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
+      },
+    })
+    .then(res => {
+      if (!res.ok) {
+        return res.json().then(e => Promise.reject(e))
+      }
+      return res.json()
+    })
+    .then(items => {
+      console.log(items)
+      this.setState({
+        words: items.words,
+      })
+    })
+
+  }
+
+  handleClickButton() {
+
+  }
+
   render() {
     return (
       <section>
         implement and style me
       </section>
+
     );
   }
 }

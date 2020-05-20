7b88d0ab5801f8084f40e3c03d6c478d51198d93
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-20 10:33:38 -0700

    fixed context issue

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import config from './config'
+import TokenService from './services/token-service'
 
 const WordContext = React.createContext({
     words: [], 
@@ -15,6 +17,28 @@ export class ContextProvider extends Component {
         setWords: ()=>{},
         setLanguage: ()=>{}
     }
+
+    componentDidMount() {
+        fetch(`${config.API_ENDPOINT}/language`, {
+          headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${TokenService.getAuthToken()}`
+          },
+        })
+        .then(res => {
+          if (!res.ok) {
+            return res.json().then(e => Promise.reject(e))
+          }
+          return res.json()
+        })
+        .then(items => {
+          this.setState({
+            words: items.words,
+            language: items.language
+          })
+        })
+    
+      }
 
     setWords = words => {
         this.setState({ words: words })
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,38 +1,11 @@
 import React, { Component } from 'react'
-import config from '../../config'
-import TokenService from '../../services/token-service'
 import './DashboardRoute.css'
 import Context from '../../Context'
 
 class DashboardRoute extends Component {
 
   static contextType = Context
-  // state = {
-  //   words: [],
-  //   language: {}
-  // }
-
-  componentDidMount() {
-    fetch(`${config.API_ENDPOINT}/language`, {
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${TokenService.getAuthToken()}`
-      },
-    })
-    .then(res => {
-      if (!res.ok) {
-        return res.json().then(e => Promise.reject(e))
-      }
-      return res.json()
-    })
-    .then(items => {
-      console.log('Fetched data:', items)
-      this.context.setWords({words: items.words})
-      this.context.setLanguage({language: items.language})
-      console.log('context:', this.context)
-    })
-
-  }
+ 
 
   handleClickButton = () => {
     this.props.history.push('/learn')
@@ -40,7 +13,6 @@ class DashboardRoute extends Component {
 
   renderWordList() {
     const words = this.context.words;
-    console.log('words:', words)
     const wordsList = words.map((word, index) =>
       <li key={index}>
         <h4>{word.original}</h4>
@@ -57,16 +29,17 @@ class DashboardRoute extends Component {
   }
 
   render() {
-    const value = { words: this.context.words, language: this.context.language }
+    const lang = this.context.language;
+    console.log(lang)
     return (
         <div className="Dashboard">
           <main>
-          <h2>{value.language.name}</h2>
+          <h2>{lang.name}</h2>
             <section>
               <a type='submit' href="/learn" className="practice">Start practicing</a>
             </section>
             <h3>Words to practice</h3>
-            <h3>Total correct answers: {value.language.total_score}</h3>
+            <h3>Total correct answers: {lang.total_score}</h3>
             {this.renderWordList()}
           </main>
         </div>

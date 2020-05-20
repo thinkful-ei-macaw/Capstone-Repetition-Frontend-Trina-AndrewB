035c357257f10192b6383bccf66e0f04ebd2cc10
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-20 13:20:06 -0700

    testing for learning route complete

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import config from '../../config'
 import Context from '../../Context'
-// import './LearningRoute.css'
+import './LearningRoute.css'
 import TokenService from '../../services/token-service'
 
 
 class LearningRoute extends Component {
 
   static contextType = Context
+
+  state = {
+    nextWord: "",
+    wordCorrectCount: 0,
+    wordIncorrectCount: 0,
+    totalScore: 0
+  }
 
   handleSubmitBtn = e => {
 
@@ -29,16 +36,39 @@ class LearningRoute extends Component {
     })
     .then(head => {
       console.log(head)
-      console.log(this.context)
-      //this.context.setLanguage(head)
+      this.setState(head)
     })
   }
 
   render() {
+    const { nextWord, wordCorrectCount, wordIncorrectCount, totalScore } = this.state
     return (
-      <section>
-        implement and style me
-      </section>
+      <div className="LearningPage">
+        <main>
+          <h2>Translate the word:</h2>
+          <span className="nextword">{nextWord}</span>
+          <form>
+            <fieldset>
+              <legend>
+                <label for="learn-guess-input" >What's the translation for this word?</label>
+                <input type="text" id="learn-guess-input" name="learn-guess-input" placeholder="Enter translated word here" required/>
+                <button type='submit'>Submit your answer</button>
+
+              </legend>
+            </fieldset>
+          </form>
+          <footer>
+            <section className="answered">
+              <p className="correct">Your total score is: {totalScore}</p><br/>
+              <p className="incorrect">You have answered this word <span>correctly</span> {wordCorrectCount} times.</p>
+            </section>
+            <section className="score">
+              <p>You have answered this word <span>incorrectly</span> {wordIncorrectCount} times.</p>
+            </section>
+          </footer>
+        </main>
+      </div>
+      
     );
   }
 }

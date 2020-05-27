1f3c8252acacf224e95ab4f42f87bc4bd0b926d3
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-26 17:52:57 -0700

    pushed for help purposes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'https://glacial-atoll-99069.herokuapp.com',
+  // API_ENDPOINT: 'https://glacial-atoll-99069.herokuapp.com',
+  API_ENDPOINT: 'http://localhost:8000',
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 }
 
